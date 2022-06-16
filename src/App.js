07b1a6a0ef2db33e6db44b6759f8a888012d0b3c
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-06-16 06:52:24 +0530

    game structer added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Square extends Component {
+  render() {
+    return <button style={{ width: "30px", height: "30px", margin: "5px" }}></button>;
+  }
 }
-
-export default App;
+class Board extends Component {
+  renderSquare() {
+    return <Square />;
+  }
+  render() {
+    return (
+      <div>
+        <div>
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div>
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div>
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Board />
+      </div>
+    );
+  }
+}
