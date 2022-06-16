617e2a9f3ac4f9b246bf361f37892f65f1280d30
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-06-16 07:42:58 +0530

    Click event added for button to put x or o

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React, { Component } from "react";
 
 class Square extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
   render() {
     return (
-      <button className="btn btn-sm btn-outline-secondary m-2 fs-1" style={{ width: "75px", height: "75px" }} onClick={() => this.setState({ value: "X" })}>
-        {this.state.value}
+      <button className="btn btn-sm btn-outline-secondary m-2 fs-1" style={{ width: "75px", height: "75px" }} onClick={this.props.onClick}>
+        {this.props.value}
       </button>
     );
   }
 }
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+  handleClick(i) {
+    //clone of current array
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    //updated new array with current array
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
   render() {
     return (
